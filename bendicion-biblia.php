0592738f5bc975d201c14a48feafe8990f7c8506
Bendicion.net <Bendicion.net@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-26 01:54:32 +0000

    Version 1.0 This plugin installs the Spanish Bible 1960 version and allows the user to  search the Bible by book, chapter, verse and words.
    git-svn-id: https://plugins.svn.wordpress.org/biblia-y-concordancia/trunk@731829 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bendicion-biblia.php
@@ -0,0 +1,443 @@
+<? /*
+
+   Plugin Name: Biblia y Concordancia
+
+   Plugin URI:  http://bendicion.net/biblia-para-wordpress/
+
+   Description: Busqueda y concordancia de la Biblia RV 1960 © American Bible Society.
+
+   Version: 1.0
+
+   Author: Bendicion.net - Arlo B. Calles
+
+   Author URI: http://bendicion.net
+
+   License: GPL2
+
+
+
+   Copyright 2013  Bendicion.net - Arlo Calles  (email : arlo@bendicion.net)
+
+   This program is free software; you can redistribute it and/or modify
+
+   it under the terms of the GNU General Public License as published by
+
+   the Free Software Foundation; either version 2 of the License, or
+
+   (at your option) any later version.
+
+   
+
+   This program is distributed in the hope that it will be useful,
+
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+   GNU General Public License for more details.
+
+   
+
+   You should have received a copy of the GNU General Public License
+
+   along with this program; if not, write to the Free Software
+
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   */
+
+global $bendicion_biblia_db_version;
+
+$bendicion_biblia_db_version = "1.0";
+
+
+
+### Create widget
+
+function widget_bendicion_biblia() {
+
+  echo '<p><b>Biblia y Concordancia</b></p>';
+
+  display_bible_form();
+
+}
+
+// Register and begin widget 
+
+function bendicion_biblia_init()
+
+{
+
+  register_sidebar_widget(__('Biblia y Concordancia'), 'widget_bendicion_biblia');     
+
+}
+
+add_action("plugins_loaded", "bendicion_biblia_init");
+
+
+
+// Run function when the plugin is activated
+
+register_activation_hook(__FILE__, 'bendicion_biblia_install');
+
+
+
+### Create table and insert data
+
+function bendicion_biblia_install()
+
+{
+
+	global $wpdb;
+
+	global $bendicion_biblia_db_version;
+
+	$table_name = $wpdb->prefix . "bendicion_biblia";
+
+	// If table doesn't exist
+
+	if($wpdb->get_var("show tables like $table_name") != $table_name)
+
+	{
+
+    // Import data file
+
+	require_once(ABSPATH . 'wp-content/plugins/Bendicion-Biblia/bendicion_biblia_database.php');
+
+	
+
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+	dbDelta($sql);
+
+	add_option("bendicion_biblia_db_version", $bendicion_biblia_db_version);
+
+	}
+
+}
+
+
+
+// Run function when the plugin is deactivated
+
+register_deactivation_hook(__FILE__, 'bendicion_biblia_uninstall');
+
+### Delete table
+
+function bendicion_biblia_uninstall()
+
+{
+
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . "bendicion_biblia";
+
+	$bible_delete_table = $wpdb->get_results( $wpdb->prepare("DROP TABLE $table_name") );
+
+}
+
+
+
+### Function: Short Code For adding the Bible into a Page
+
+add_shortcode('bendicion_biblia', 'bible_page_shortcode');
+
+function bible_page_shortcode($atts) {
+
+	return display_bible_form();
+
+}
+
+### Function: Display Bible Form
+
+function display_bible_form() {
+
+$highlight_color = 'ffff00';
+
+### Print Search Text Form
+
+  echo "\t<form id=\"\" class=\"bendicion-bible\" action=\"".htmlspecialchars($_SERVER['REQUEST_URI'])."\" method=\"post\">\n";
+
+  echo "<select name=\"libro\" size=\"1\">\n";
+
+  echo "<option value=\"\" selected=\"selected\">Seleccionar Libro</option>\n";  
+
+  echo "<option value=\"1\">G&eacute;nesis</option>\n";
+
+  echo "<option value=\"2\">&Eacute;xodo</option>\n";
+
+  echo "<option value=\"3\">Lev&iacute;tico</option>\n";
+
+  echo "<option value=\"4\">N&uacute;meros</option>\n";
+
+  echo "<option value=\"5\">Deuteronomio</option>\n";
+
+  echo "<option value=\"6\">Josu&eacute;</option>\n";
+
+  echo "<option value=\"7\">Jueces</option>\n";
+
+  echo "<option value=\"8\">Rut</option>\n";
+
+  echo "<option value=\"9\">1 Samuel</option>\n";
+
+  echo "<option value=\"10\">2 Samuel</option>\n";
+
+  echo "<option value=\"11\">1 Reyes</option>\n";
+
+  echo "<option value=\"12\">2 Reyes</option>\n";
+
+  echo "<option value=\"13\">1 Cr&oacute;nicas</option>\n";
+
+  echo "<option value=\"14\">2 Cr&oacute;nicas</option>\n";
+
+  echo "<option value=\"15\">Esdras</option>\n";
+
+  echo "<option value=\"16\">Nehem&iacute;as</option>\n";
+
+  echo "<option value=\"17\">Ester</option>\n";
+
+  echo "<option value=\"18\">Job</option>\n";
+
+  echo "<option value=\"19\">Salmos</option>\n";
+
+  echo "<option value=\"20\">Proverbios</option>\n";
+
+  echo "<option value=\"21\">Eclesiast&eacute;s</option>\n";
+
+  echo "<option value=\"22\">Cantares</option>\n";
+
+  echo "<option value=\"23\">Isa&iacute;as</option>\n";
+
+  echo "<option value=\"24\">Jerem&iacute;as</option>\n";
+
+  echo "<option value=\"25\">Lamentaciones</option>\n";
+
+  echo "<option value=\"26\">Ezequiel</option>\n";
+
+  echo "<option value=\"27\">Daniel</option>\n";
+
+  echo "<option value=\"28\">Oseas</option>\n";
+
+  echo "<option value=\"29\">Joel</option>\n";
+
+  echo "<option value=\"30\">Am&oacute;s</option>\n";
+
+  echo "<option value=\"31\">Abd&iacute;as</option>\n";
+
+  echo "<option value=\"32\">Jon&aacute;s</option>\n";
+
+  echo "<option value=\"33\">Miqueas</option>\n";
+
+  echo "<option value=\"34\">Nah&uacute;m</option>\n";
+
+  echo "<option value=\"35\">Habacuc</option>\n";
+
+  echo "<option value=\"36\">Sofon&iacute;as</option>\n";
+
+  echo "<option value=\"37\">Hageo</option>\n";
+
+  echo "<option value=\"38\">Zacar&iacute;as</option>\n";
+
+  echo "<option value=\"39\">Malaqu&iacute;as</option>\n";
+
+  echo "<option value=\"40\">Mateo</option>\n";
+
+  echo "<option value=\"41\">Marcos</option>\n";
+
+  echo "<option value=\"42\">Lucas</option>\n";
+
+  echo "<option value=\"43\">Juan</option>\n";
+
+  echo "<option value=\"44\">Hechos</option>\n";
+
+  echo "<option value=\"45\">Romanos</option>\n";
+
+  echo "<option value=\"46\">1 Corintios</option>\n";
+
+  echo "<option value=\"47\">2 Corintios</option>\n";
+
+  echo "<option value=\"48\">G&aacute;latas</option>\n";
+
+  echo "<option value=\"49\">Efecios</option>\n";
+
+  echo "<option value=\"50\">Filipenses</option>\n";
+
+  echo "<option value=\"51\">Colosenses</option>\n";
+
+  echo "<option value=\"52\">1 Tesalonicenses</option>\n";
+
+  echo "<option value=\"53\">2 Tesalonicenses</option>\n";
+
+  echo "<option value=\"54\">1 Timoteo</option>\n";
+
+  echo "<option value=\"55\">2 Timoteo</option>\n";
+
+  echo "<option value=\"56\">Tito</option>\n";
+
+  echo "<option value=\"57\">Filem&oacute;n</option>\n";
+
+  echo "<option value=\"58\">Hebreos</option>\n";
+
+  echo "<option value=\"59\">Santiago</option>\n";
+
+  echo "<option value=\"60\">1 Pedro</option>\n";
+
+  echo "<option value=\"61\">2 Pedro</option>\n";
+
+  echo "<option value=\"62\">1 Juan</option>\n";
+
+  echo "<option value=\"63\">2 Juan</option>\n";
+
+  echo "<option value=\"64\">3 Juan</option>\n";
+
+  echo "<option value=\"65\">Judas</option>\n";
+
+  echo "<option value=\"66\">Apocalipsis</option>\n";
+
+  echo "</select>\n";
+
+  echo "\t\t<input type=\"text\" name=\"capitulo\" maxlength=\"3\" size=\"4\" onBlur=\"if (value == '') {value = 'cap&iacute;tulo'}\" 
+
+                        onFocus=\"if (value == 'cap&iacute;tulo') {value =''}\"
+
+                        value=\"cap&iacute;tulo\" />"."\n";
+
+  echo "\t\t<input type=\"text\" name=\"versiculo\" maxlength=\"3\" size=\"4\" onBlur=\"if (value == '') {value = 'vers&iacute;culo'}\" 
+
+                        onFocus=\"if (value == 'vers&iacute;culo') {value =''}\"
+
+                        value=\"vers&iacute;culo\" />"."\n";
+
+  echo "\t\t<input type=\"submit\" name=\"Submit\" value=\"Buscar\" />\n";
+
+  echo "\t</form>\n";
+
+### Print Concordancia Form
+
+  echo "\t<form id=\"\" class=\"bendicion-bible\" action=\"".htmlspecialchars($_SERVER['REQUEST_URI'])."\" method=\"post\">\n";
+
+  echo "\t\t<input type=\"text\" name=\"palabras\" maxlength=\"50\" onBlur=\"if (value == '') {value = 'Por palabras'}\"
+
+                        onFocus=\"if (value == 'Por palabras') {value =''}\"
+
+                        value=\"Por palabras\" />"."\n";
+
+  echo "\t\t<input type=\"submit\" name=\"Submit\" value=\"Buscar\" />\n";
+
+  echo "\t</form>\n";
+
+### Determine if Bible text was received
+
+if (isset($_POST['libro']) && !empty($_POST['libro']) && isset($_POST['capitulo']) && !empty($_POST['capitulo']) && isset($_POST['versiculo']) && !empty($_POST['versiculo']))
+
+{
+
+   // If the user just sent info
+
+   $libro = $_POST['libro'];
+
+   $capitulo = $_POST['capitulo'];
+
+   $versiculo = $_POST['versiculo'];
+
+global $wpdb;
+
+$table_name = $wpdb->prefix.'bendicion_biblia';
+
+$bible_result = $wpdb->get_results( $wpdb->prepare("SELECT * FROM $table_name WHERE libro=$libro AND capitulo=$capitulo AND versiculo=$versiculo") );
+
+$bible_result2 = $wpdb->get_results( $wpdb->prepare("SELECT * FROM $table_name WHERE libro=0 AND capitulo=10 AND versiculo=$libro") );
+
+// Get Bible Text
+
+foreach($bible_result as $bible_text) {
+
+			$bible_text = $bible_text->texto;
+
+       }
+
+// Get Bible Book
+
+foreach($bible_result2 as $bible_book) {
+
+			$bible_book = $bible_book->texto;
+
+       }
+
+// Display All
+
+echo $bible_text.'&nbsp;'.$bible_book.'&nbsp;'.$capitulo.':'.$versiculo.'</br></br>Citas b&iacute;blicas tomadas de la <b>Reina-Valera 1960 (RVR1960)</b></br>Copyright © 1960 by American Bible Society.';
+
+}
+
+### Determine if Concordancia was received
+
+else if (isset($_POST['palabras']) && !empty($_POST['palabras']))
+
+{
+
+   // If the user just sent info
+
+   $palabras = stripslashes($_POST['palabras']);
+
+   // Save the search term in this varibale to be able to use it in the output
+
+   $palabra = $palabras;
+
+   // Add wildcard
+
+   $palabras = "%" . $palabras . "%";
+
+   // Connect to the database
+
+   global $wpdb;
+
+   $table_name = $wpdb->prefix.'bendicion_biblia';
+
+   $search_text = $wpdb->get_results( $wpdb->prepare("SELECT * FROM $table_name WHERE texto LIKE %s", $palabras) );
+
+   // Loop for results
+
+   foreach ($search_text as $single_text) {
+
+     $output = $single_text->texto;
+
+     $libro = $single_text->libro;
+
+	 $capitulo = $single_text->capitulo;
+
+	 $versiculo = $single_text->versiculo;
+
+     $output = str_replace($palabra,"<span style=background-color:yellow>".$palabra."</span>",$output);
+
+	 // Find the name of the book by looking up the number
+
+	 $bible_result2 = $wpdb->get_results( $wpdb->prepare("SELECT * FROM $table_name WHERE libro=0 AND capitulo=10 AND versiculo=$libro") );
+
+	 // Get Bible Book
+
+     foreach($bible_result2 as $bible_book) {
+
+			$bible_book = $bible_book->texto;
+
+     }
+
+	 // Display search results
+
+	 echo '<p>'.$output.'&nbsp;'.$bible_book.'&nbsp;'.$capitulo.':'.$versiculo.'</p>';
+
+	 $count = $count + 1;
+
+   }
+
+   echo '<b>N&uacute;mero de vers&iacute;culos encontrados:</b> '.$count.'</br></br>Citas b&iacute;blicas tomadas de la <b>Reina-Valera 1960 (RVR1960)</b></br>Copyright © 1960 by American Bible Society.';
+
+}
+
+}
+
+return $display_bible_form;
+
+
+
+$wpdb->flush();?>
---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== Biblia y Concordancia ===
+Contributors: bendicion.net
+Donate link: http://bendicion.net/
+Tags: biblia, bible
+Requires at least: 3.0.1
+Stable tag: 1.0
+Tested up to: 3.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin installs the Spanish Bible 1960 version and allows the user to search the Bible by book, chapter, verse and words.
+
+== Description ==
+
+Este plugin de WordPress instala la Biblia en español versión Reina Valera 1960 © American Bible Society, y permite
+al usuario buscar en la Biblia por medio de libro, capítulo, versículo y concordancia.
+
+== Installation ==
+
+1. Upload the plugin `biblia y concordancia` to the `/wp-content/plugins/` directory
+    Sube el plugin `biblia y concordancia` al directorio `/wp-content/plugins/`
+2. Activate the plugin through the 'Plugins' menu in WordPress
+    Activa el plugin por medio del menú 'Plugins' en WordPress
+3. Place the shortcode `[bendicion_biblia]` in a new page
+    Agrega el código `[bendicion_biblia]` en una página nueva
+4. You may also use this line in your template:
+    También puede ser activado al agregar este código en un template:
+   `<?php if (function_exists('display_bible_form')) { display_bible_form(); } ?>`
+
+== Frequently Asked Questions ==
+   Preguntas Frecuentes
+
+= If I have a technical question about installing it  =
+  Si tengo una pregunta técnica de la instalación
+
+You may contact us to info[at]bendicion.net
+Puede contactarnos en info[arroba]bendicion.net
+
+= 1.0 =
+* Using the RV1960 version.
+* Search by word, verse and chapter.
+
+== Upgrade Notice == 
+
+No changes yet
+
+== Screenshots ==
+
+No screenshots yet
